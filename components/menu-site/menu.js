824553c8b3d8a9b88d7db10a1c586dc9d4226145
mansoor <mansoor20ghafoori04@gmail.com>
2025-07-15 07:36:16 +0330

    edit menu

--- components/menu-site/menu.js
+++ components/menu-site/menu.js
@@ -11,16 +11,24 @@ class MenuSite extends HTMLElement {
   createTemplate() {
     let template = $.createElement("template");
     template.innerHTML = `
-${sheet}
+    ${sheet}
 <link rel="stylesheet" href="./components/menu-site/style-menu.css" />
- <nav class="toolbar nav-link">
-      <p class="logo set-font">BURGER HEAVEN</p>
-      <ul class="menu nav-link">
-        <li><a href="#" class="items set-font">ABOUT</a></li>
-        <li><a href="#" class="items set-font">MENU</a></li>
-        <li><a href="#" class="order set-font">ORDER ONLINE</a></li>
-      </ul>
-    </nav>
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=shopping_cart"
+/>
+<nav class="toolbar nav-link">
+  <div class="shopping-cart">
+    <span class="material-symbols-outlined"> shopping_cart </span>
+    <span id="counter">0</span>
+  </div>
+  <p class="logo set-font">BURGER HEAVEN</p>
+  <ul class="menu nav-link">
+    <li><a href="#" class="items set-font">ABOUT</a></li>
+    <li><a href="#" class="items set-font">MENU</a></li>
+    <li><a href="#" class="order set-font">ORDER ONLINE</a></li>
+  </ul>
+</nav>
     `;
     return template.content.cloneNode(true);
   }
