2eeca370cf97ad800d03c6ccbe2d01ec646ed5f9
mansoor <mansoor20ghafoori04@gmail.com>
2025-07-18 13:22:00 +0330

    responsiv menu

--- components/menu-site/menu.js
+++ components/menu-site/menu.js
@@ -1,36 +1,32 @@
 import { sheet } from "/js/global-style.js";
 let $ = document;
 
+let template = $.createElement("template");
+template.innerHTML = `
+${sheet}
+<link rel="stylesheet" href="./components/menu-site/style-menu.css" />
+<link
+rel="stylesheet"
+href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+/>
+<nav class="toolbar nav-link">
+<div class="shopping-cart">
+<span class="material-symbols-outlined cart-icon"> shopping_cart </span>
+<span id="counter">0</span>
+</div>
+<p class="logo set-font">BURGER HEAVEN</p>
+<ul class="menu">
+<li><a href="#" class="items set-font">ABOUT</a></li>
+<li><a href="#" class="order set-font">ORDER ONLINE</a></li>
+</ul>
+</nav>
+`;
+
 class MenuSite extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-
-    this.shadowRoot.appendChild(this.createTemplate());
-  }
-  createTemplate() {
-    let template = $.createElement("template");
-    template.innerHTML = `
-    ${sheet}
-<link rel="stylesheet" href="./components/menu-site/style-menu.css" />
-<link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=shopping_cart"
-/>
-<nav class="toolbar nav-link">
-  <div class="shopping-cart">
-    <span class="material-symbols-outlined"> shopping_cart </span>
-    <span id="counter">0</span>
-  </div>
-  <p class="logo set-font">BURGER HEAVEN</p>
-  <ul class="menu nav-link">
-    <li><a href="#" class="items set-font">ABOUT</a></li>
-    <li><a href="#" class="items set-font">MENU</a></li>
-    <li><a href="#" class="order set-font">ORDER ONLINE</a></li>
-  </ul>
-</nav>
-    `;
-    return template.content.cloneNode(true);
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
 
