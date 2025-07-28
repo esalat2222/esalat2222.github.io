b14ff833c65d3420ba4a5efdbb85c07639c58c92
mansoor <mansoor20ghafoori04@gmail.com>
2025-07-28 09:16:11 +0330

    delete two options from menu and delete privecy

--- components/footer-site/footer.js
+++ components/footer-site/footer.js
@@ -8,35 +8,21 @@ ${sheet}
 <footer>
       <div class="foot-box">
         <p class="title-foot">Burger Heaven</p>
-        <p>Copyright Burger Heaven</p>
+        <p style="font-size:1vw;">Copyright Burger Heaven</p>
       </div>
       <div class="foot-box">
         <p class="title-foot">more</p>
         <ul>
-          <li><a href="#">About</a></li>
-        </ul>
-        <ul>
-          <li><a href="#">Menu</a></li>
-        </ul>
-        <ul>
-          <li><a href="#">Location</a></li>
-        </ul>
-        <ul>
-          <li><a href="#">Privacy</a></li>
+          <li><a href="#" class="menu-list">About</a></li>
+          <li><a href="#" class="menu-list">Location</a></li>
         </ul>
       </div>
-      <div id="box-time">
-        <p class="title-foot">Hours</p>
-        <p>Open from</p>
-        <p>11AM to</p>
-        <p>11PM daily</p>
-      </div>
+      
       <div class="foot-box">
         <div>
           <p class="title-foot">Join Our Newsletter</p>
           <button id="join-btn">SUBMIT</button>
         </div>
-        <p id="pr-po">Privacy Policy</p>
       </div>
     </footer>
 `;
